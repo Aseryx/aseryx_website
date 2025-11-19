bd7b84b97fe52023243fd2b7fdb3d2ca513befde
Taladhak <talabiouhd20@gmail.com>
2025-11-19 13:15:01 -0500

    feat: update App.jsx to render single-page LandingPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Members from './pages/Members'
-import Partners from './pages/Partners'
-import Terms from './pages/Terms'
+import React from 'react'
+import LandingPage from './pages/LandingPage'
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-gray-50">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/members" element={<Members />} />
-            <Route path="/partners" element={<Partners />} />
-            <Route path="/terms" element={<Terms />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <LandingPage />
   )
 }
 
